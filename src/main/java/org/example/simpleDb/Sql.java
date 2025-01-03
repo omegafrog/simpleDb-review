b806f90416b3b921f6914dcefe08f47086c78209
omegafrog <jiwoo22110103@gmail.com>
2025-01-03 09:09:09 +0900

    feat: Sql.selectLong() 작성

--- src/main/java/org/example/simpleDb/Sql.java
+++ src/main/java/org/example/simpleDb/Sql.java
@@ -116,6 +116,14 @@ public class Sql {
     }
 
     public Long selectLong() {
+        try(Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery(s.toString());
+            while(rs.next()){
+                return rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
